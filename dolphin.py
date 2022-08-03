003e96099817024b995a7ea47abcc6e00df39655
Troy Dutton <THESNEAKYCRAFTER@gmail.com>
2022-08-02 21:02:46 -0500

    Ultrasonic GPIO

--- dolphin.py
+++ dolphin.py
@@ -1,10 +1,73 @@
+# dolpin.py
+# Author: Troy Dutton
+# Date Modfied: August 1, 2022
+
+from math import dist
 from socket import *
-HOST = "192.168.1.2"
+import select
+import RPi.GPIO as gpio
+import time
+
+HOST = "192.168.0.27"
 PORT = 12000
-s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT))
-messagetoserver = input("Enter Message for Server: ")
-s.send(messagetoserver)
-replyfromserver = s.recv(1024)
-print ("Reply Message from Server: ", replyfromserver)
-s.close()
+SPEED_OF_SOUND = 34300
+ULTRASONIC_OUT_PINS = [15]
+ULTRASONIC_IN_PINS = [13]
+MOTOR_DIR_PINS = []
+MOTOR_STEP_PINS = []
+
+def wifiInit():
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.connect((HOST, PORT))
+    return server_socket
+
+def gpioInit():
+    gpio.setmode(gpio.BOARD)
+
+    # Setup ultrasonic sensors
+    for pin in ULTRASONIC_OUT_PINS:
+        gpio.setup(pin, gpio.OUT)
+    
+    for pin in ULTRASONIC_IN_PINS:
+        gpio.setup(pin, gpio.IN)
+
+    # Setup stepper motors
+    for pin in MOTOR_DIR_PINS:
+        gpio.setup(pin, gpio.OUT)
+    
+    for pin in MOTOR_STEP_PINS:
+        gpio.setup(pin, gpio.OUT)
+
+def measureDistance():
+    distances = []
+    for trig, echo in zip(ULTRASONIC_OUT_PINS, ULTRASONIC_IN_PINS):
+        # Pulse trigger pin
+        gpio.output(trig, gpio.HIGH)
+        time.sleep(0.00001)
+        gpio.output(trig, gpio.LOW)
+
+        # Start timer and wait for signal to be recieved
+        start_t = time.perf_counter()
+        gpio.wait_for_edge(echo, gpio.RISING)
+
+        # Stop timer and calculate distance traveled (cm)
+        elapsed_t = time.perf_counter() - start_t
+        distance = (elapsed_t * SPEED_OF_SOUND) / 2
+        distances.append(distance)
+    return distances
+
+
+
+def main():
+    gpioInit()
+    server_socket = wifiInit()
+
+    while True:
+        r, w, e = select.select([server_socket], [], [], .01)
+        if (r): # If there is data available
+            print(server_socket.recv(1024).decode())
+
+
+
+if __name__ == "__main__":
+    main()
